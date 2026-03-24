82f979ea1c81e28a935c3d9c4e872e9b74a39767
Pedro <p.miraspc35@gmail.com>
2026-03-24 09:32:14 +0100

    Separacion en subcomponetes

--- sushi-tracker/src/components/SushiCounter copy.jsx
+++ sushi-tracker/src/components/SushiCounter copy.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import niguiriImage from '../assets/images/niguiri.png';
 import niguiriImage1 from '../assets/images/niguiri1.png';
-import Header from './Header';
 
 /* FUNCIONES AUXILIARES (HELPER FUNCTIONS) */
 
@@ -158,11 +157,20 @@ export default function SushiCounter({ playerName, onUpdateName }) {
         <div className="flex flex-col h-full min-h-screen bg-black relative overflow-hidden text-white">
 
             {/* BARRA DE NAVEGACIÓN (HEADER) */}
-            <Header
-                playerName={playerName}
-                onOpenSettings={() => setIsSettingsOpen(true)}
-                onOpenHistory={() => setIsMenuOpen(true)}
-            />
+            <header className="flex justify-between items-center p-6 bg-black border-b border-black">
+                <button onClick={() => setIsSettingsOpen(true)} className="text-gray-400 hover:bg-gray-800 hover:text-white p-2 rounded-xl transition-colors cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.528.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                </button>
+                <h2 className="text-xl font-black text-white">{playerName}</h2>
+                <button onClick={() => setIsMenuOpen(true)} className="text-purple-400 hover:bg-gray-800 hover:text-purple-500 p-2 rounded-xl transition-colors cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-7 h-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
+            </header>
 
             {/* CONTENIDO PRINCIPAL (MAIN VIEW) */}
             <main className="flex-1 flex flex-col items-center justify-center p-6 text-center">
